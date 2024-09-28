f3aec22e92f083f043d8595b065e033e15ca98bd
sadeval <162736837+sadeval@users.noreply.github.com>
2024-09-28 16:27:16 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace TextAnalyzerApp
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.RadioButton rbtnShowOnScreen;
+        private System.Windows.Forms.RadioButton rbtnSaveToFile;
+        private System.Windows.Forms.TextBox txtReport;
+
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.rbtnShowOnScreen = new System.Windows.Forms.RadioButton();
+            this.rbtnSaveToFile = new System.Windows.Forms.RadioButton();
+            this.txtReport = new System.Windows.Forms.TextBox();
+
+            this.SuspendLayout();
+
+            // TextBox для ввода текста
+            this.txtInput.Location = new System.Drawing.Point(12, 12);
+            this.txtInput.Multiline = true;
+            this.txtInput.Size = new System.Drawing.Size(400, 100);
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+
+            // Кнопка "Анализировать"
+            this.btnAnalyze.Location = new System.Drawing.Point(12, 120);
+            this.btnAnalyze.Text = "Анализировать";
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+
+            // Радиокнопка для вывода на экран
+            this.rbtnShowOnScreen.Location = new System.Drawing.Point(12, 150);
+            this.rbtnShowOnScreen.Text = "Вывод на экран";
+            this.rbtnShowOnScreen.Checked = true;
+
+            // Радиокнопка для сохранения в файл
+            this.rbtnSaveToFile.Location = new System.Drawing.Point(150, 150);
+            this.rbtnSaveToFile.Text = "Сохранить в файл";
+
+            // TextBox для отображения отчета
+            this.txtReport.Location = new System.Drawing.Point(12, 180);
+            this.txtReport.Multiline = true;
+            this.txtReport.Size = new System.Drawing.Size(400, 200);
+            this.txtReport.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+
+            // Настройки формы
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnAnalyze);
+            this.Controls.Add(this.rbtnShowOnScreen);
+            this.Controls.Add(this.rbtnSaveToFile);
+            this.Controls.Add(this.txtReport);
+            this.Text = "Анализатор текста";
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextAnalyzerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            string text = txtInput.Text;
+
+            var report = await Task.Run(() => AnalyzeText(text));
+
+            if (rbtnShowOnScreen.Checked)
+            {
+                txtReport.Text = report;
+            }
+            else if (rbtnSaveToFile.Checked)
+            {
+                SaveReportToFile(report);
+                MessageBox.Show("Отчет сохранен в файл.");
+            }
+        }
+
+        private string AnalyzeText(string text)
+        {
+            int sentenceCount = Regex.Matches(text, @"[.!?]").Count; 
+            int charCount = text.Length;                             
+            int wordCount = Regex.Matches(text, @"\b\w+\b").Count;  
+            int questionCount = Regex.Matches(text, @"\?").Count;    
+            int exclamationCount = Regex.Matches(text, @"!").Count;  
+
+            return $"Количество предложений: {sentenceCount}\r\n" +
+                   $"Количество символов: {charCount}\r\n" +
+                   $"Количество слов: {wordCount}\r\n" +
+                   $"Количество вопросительных предложений: {questionCount}\r\n" +
+                   $"Количество восклицательных предложений: {exclamationCount}\r\n";
+        }
+
+        private void SaveReportToFile(string report)
+        {
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "TextReport.txt");
+            File.WriteAllText(filePath, report);
+        }
+    }
+}
